f48ffc135678e489a326d8e47b2c883775269b8b
Clinton <clintonfham@gmail.com>
2023-01-26 21:02:48 -0500

    first commit

---
+++ src/main/java/com/cpan252/tekkenreborn/TekkenrebornApplication.java
@@ -0,0 +1,14 @@
+package com.cpan252.tekkenreborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class TekkenrebornApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext  context = SpringApplication.run(TekkenrebornApplication.class, args);
+	}
+
+}
